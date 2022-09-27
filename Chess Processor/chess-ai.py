af26f151984bb1d88b628ac351e19fc20d7f480e
69VR69 <44296242+69VR69@users.noreply.github.com>
2022-09-27 22:31:28 +0200

    Create global structure

--- Chess Processor/chess-ai.py
+++ Chess Processor/chess-ai.py
@@ -16,17 +16,33 @@ def compare(fen1, fen2):
         if lines1[i] == lines2[i]:
             continue
         else:
-            print("line " + str(9-i))
+            print("line " + str(9 - i))
             print(lines1[i] + " != " + lines2[i])
 
     return moveplayed
 
 
+def detect_not_legal_move(fen):
+    return {}
+
+
+def detect_end_of_game(fen):
+    return {}
+
+
+def find_next_move(fen):
+    stockfish = Stockfish('stockfish_15_win_x64_popcnt/stockfish_15_win_x64_popcnt/stockfish_15_x64_popcnt.exe')
+    stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    return stockfish.get_best_move()
+
+
+def play_solo():
+    return {}
+
+
 if __name__ == '__main__':
     print("")
-    stockfish = Stockfish('stockfish_15_win_x64_popcnt/stockfish_15_win_x64_popcnt/stockfish_15_x64_popcnt.exe')
-    # stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    # print(stockfish.get_best_move())
+
     fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
     fen2 = "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1"
     print(compare(fen1, fen2))
