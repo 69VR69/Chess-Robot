3a2c2d40de747ab0249b048e5c145eb99aa3ec88
69VR69 <44296242+69VR69@users.noreply.github.com>
2022-09-29 22:28:17 +0200

    Improve FEN compare

--- Chess Processor/chess-ai.py
+++ Chess Processor/chess-ai.py
@@ -2,24 +2,41 @@ from stockfish import Stockfish
 
 
 def compare(fen1, fen2):
-    moveplayed = ""
+    move_played = ""
 
     lines1 = fen1.split(" ")[0].split("/", 8)
     lines2 = fen2.split(" ")[0].split("/", 8)
 
-    print(fen1)
-    print(lines1)
-    print(fen2)
-    print(lines2)
-
-    for i in range(len(lines1)):
+    for i in reversed(range(len(lines1))):
         if lines1[i] == lines2[i]:
             continue
         else:
-            print("line " + str(9 - i))
-            print(lines1[i] + " != " + lines2[i])
+            # print("line1: " + lines1[i])
+            # print("line2: " + lines2[i])
+            temp1 = stringify_fen(lines1[i])
+            temp2 = stringify_fen(lines2[i])
+            for j in range(len(temp1)):
+                if temp1[j] == temp2[j]:
+                    continue
+                else:
+                    print("move: (" + str(chr(65 + j)) + "," + str(8-i)+")")
+                    if temp1[j] != "." or temp2[j] != ".":
+                        move_played += str(chr(65 + j)) + str(8 - i)
+    return move_played
 
-    return moveplayed
+
+def stringify_fen(fen_part):
+    result = ""
+    if fen_part == "":
+        return result
+    else:
+        for fp in fen_part:
+            if fp.isdigit():
+                for j in range(int(fp)):
+                    result = result + "."
+            else:
+                result = result + fp
+        return result
 
 
 def detect_not_legal_move(fen):
@@ -43,6 +60,9 @@ def play_solo():
 if __name__ == '__main__':
     print("")
 
-    fen1 = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-    fen2 = "rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3 0 1"
+    fen1 = "qbrkbnrn/pppppppp/8/8/8/8/PPPPPPPP/QBRKBNRN w - - 0 1"
+    fen2 = "qbrkbnrn/pppppppp/8/8/8/3P4/PPP1PPPP/QBRKBNRN b - - 0 1"
+    fen3 = "qbrkbnrn/ppppp1pp/5p2/8/8/3P4/PPP1PPPP/QBRKBNRN w - - 0 1"
     print(compare(fen1, fen2))
+    print(compare(fen2, fen3))
+    print(compare(fen1, fen3))
